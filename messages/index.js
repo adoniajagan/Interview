eac9082da986cbddbf26075e23494f75392d27e4
adoniajagan <bjagan@ventechsolutions.com>
2017-06-14 19:25:17 +0530

    aa

--- messages/index.js
+++ messages/index.js
@@ -166,7 +166,7 @@ bot.dialog('/', [
         session.send("Hi... I am the GMI_Bot. Thanks for installing me. ");		
 		request = new sql.Request();
 		request.query("Select Max(UserID) as UserID from SalesLT.Log")
-		.then(function (err, result) {
+		.then(function (result) {
 				session.send(""+results.rows.length);
 				 for (var i = 0; i < results.rows.length; i++) {
             
